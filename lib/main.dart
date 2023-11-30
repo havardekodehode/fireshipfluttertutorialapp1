a74e8065ed41c1d6f6a8ed3355b4466692dc0201
havardekodehode <havarde.kodehode@gmail.com>
2023-11-30 09:28:01 +0100

    Rows and columns, mainAxis- and crossAxisAlignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,19 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text('Flutter is Fun!'),
-        ),
-        body: Container(
-          child: const Text('Hey mom!'),
-          margin: EdgeInsets.all(100),
-          padding: EdgeInsets.all(10),
-          color: Colors.red,
-          height: 100,
-          width: 100,
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: const Text('Flutter is Fun!'),
+          ),
+          body: Column(
+            children: [
+              Icon(Icons.backpack),
+              Icon(Icons.leaderboard),
+              Icon(Icons.person),
+            ],
+          )),
     );
   }
 }
